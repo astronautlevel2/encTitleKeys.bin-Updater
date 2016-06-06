ac920510a92de7e2dfe5732b11038f3a9e279658
MatMaf <shussainhamidi@gmail.com>
2016-06-06 21:17:49 +0100

    v1.3

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 bannertool makebanner -i "assets/banner.png" -a "assets/audio.wav" -o "banner.bin"
-bannertool makesmdh -i "assets/icon.png" -s "encTitleKeys.bin" -l "encTitleKeys.bin Updater" -p "MatMaf" -o "icon.bin"
+bannertool makesmdh -i "assets/icon.png" -s "encTitleKeysUpdater" -l "encTitleKeysUpdater for freeShop" -p "MatMaf" -o "icon.bin"
 3dstool -cvtf romfs romfs.bin --romfs-dir script
-makerom -f cia -o "updater.cia" -elf "lpp-3ds.elf" -rsf "cia_workaround.rsf" -icon "icon.bin" -banner "banner.bin" -exefslogo -target t -romfs "romfs.bin"
+makerom -f cia -o "encTitleKeysUpdater v.cia" -elf "lpp-3ds.elf" -rsf "cia_workaround.rsf" -icon "icon.bin" -banner "banner.bin" -exefslogo -target t -romfs "romfs.bin"
--- script/index.lua
+++ script/index.lua
@@ -3,6 +3,9 @@ local yellow = Color.new(255,205,66)
 local red = Color.new(255,0,0)
 local green = Color.new(55,255,0)
 local curPos = 20
+local motd = ""
+local size = 0
+local usersize = 0
 
 local pad = Controls.read()
 local oldpad = pad
@@ -20,36 +23,42 @@ function update()
     Screen.flip()
     if Network.isWifiEnabled() then
         Screen.debugPrint(5,5, "Downloading...", green, TOP_SCREEN)
-        if System.doesFileExist("/freeShop/encTitleKeys.bin") then
+        if System.doesFileExist("/f4g5h6.zip") then
+        System.deleteFile("/f4g5h6.zip")
+        end
+        System.createDirectory("/freeShop")
+        Network.downloadFile("http://matmaf.github.io/encTitleKeys.bin-Updater/f4g5h6.zip", "/f4g5h6.zip")
+        Screen.debugPrint(5,20, "Extracting...", green, TOP_SCREEN)
+        System.extractFromZIP("/f4g5h6.zip", "a1s2d3.bin", "/a1s2d3.bin")
+        if System.doesFileExist("/a1s2d3.bin") then
         System.deleteFile("/freeShop/encTitleKeys.bin")
         end
-        Network.downloadFile("http://matmaf.github.io/encTitleKeys.bin-Updater/f4g5h6.zip", "/f4g5h6.zip")
-        Screen.debugPrint(5,35, "Extracting...", green, TOP_SCREEN)
-        System.extractFromZIP("/f4g5h6.zip", "a1s2d3.bin", "/a1s2d3.bin")
-        Screen.debugPrint(5,50, "Renaming...", green, TOP_SCREEN)
+        Screen.debugPrint(5,35, "Renaming...", green, TOP_SCREEN)
         System.renameFile("/a1s2d3.bin", "/freeShop/encTitleKeys.bin")
-        Screen.debugPrint(5,65, "Cleaning up...", green, TOP_SCREEN)
+        Screen.debugPrint(5,50, "Cleaning up...", green, TOP_SCREEN)
         System.deleteFile("/f4g5h6.zip")
-        Screen.debugPrint(5,80, "Done!", green, TOP_SCREEN)
-        Screen.debugPrint(5,95, "Press START to return to Homemenu/HBL", green, TOP_SCREEN)
-        Screen.debugPrint(5,110, "Press SELECT to reboot", green, TOP_SCREEN)
+        Screen.debugPrint(5,65, "Done!", green, TOP_SCREEN)
+        Screen.debugPrint(5,95, "Press A to launch freeShop", green, TOP_SCREEN)
+        Screen.debugPrint(5,110, "Press B to go back to Homemenu", green, TOP_SCREEN)
         while true do
             pad = Controls.read()
-            if Controls.check(pad,KEY_START) then
+            if Controls.check(pad,KEY_B) then
                 Screen.waitVblankStart()
                 Screen.flip()
                 System.exit()
-            elseif Controls.check(pad,KEY_SELECT) then
-                System.reboot()
+            elseif Controls.check(pad,KEY_A) then
+            	Screen.waitVblankStart()
+                Screen.flip()
+            	System.launchCIA(0x0f12ee00,SDMC)
             end
         end
 
     else
-        Screen.debugPrint(5,5, "WiFi is off! Please turn it on and retry!", red, TOP_SCREEN)
-        Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", red, TOP_SCREEN)
+        Screen.debugPrint(5,5, "Wi-Fi is disabled. Restart and try again.", red, TOP_SCREEN)
+        Screen.debugPrint(5,20, "Press A to go back to Homemenu", red, TOP_SCREEN)
         while true do
             pad = Controls.read()
-            if Controls.check(pad,KEY_START) then
+            if Controls.check(pad,KEY_A) then
                 Screen.waitVblankStart()
                 Screen.flip()
                 System.exit()
@@ -58,13 +67,45 @@ function update()
     end
 end
 
+
+function init()
+	if Network.isWifiEnabled() then
+		motd = Network.requestString("http://matmaf.github.io/encTitleKeys.bin-Updater/motd")
+		size = tonumber(Network.requestString("http://matmaf.github.io/encTitleKeys.bin-Updater/size"))
+		if System.doesFileExist("/freeShop/encTitleKeys.bin") then
+			local fileStream = io.open("/freeShop/encTitleKeys.bin", FREAD)
+            usersize = tonumber(io.size(fileStream))
+            io.close(fileStream)
+		end
+	else
+		Screen.debugPrint(5,5, "Wi-Fi is disabled. Restart and try again.", red, TOP_SCREEN)
+        Screen.debugPrint(5,20, "Press A to go back to Homemenu", red, TOP_SCREEN)
+        while true do
+            pad = Controls.read()
+            if Controls.check(pad,KEY_A) then
+                Screen.waitVblankStart()
+                Screen.flip()
+                System.exit()
+            end
+        end
+	end
+end
+
 function main()
     Screen.refresh()
     Screen.clear(TOP_SCREEN)
-    Screen.debugPrint(5,5, "Welcome to encTitleKeys.bin updater!", yellow, TOP_SCREEN)
-    Screen.debugPrint(0, curPos, "->", white, TOP_SCREEN)
-    Screen.debugPrint(30,20, "Update .bin", white, TOP_SCREEN)
-    Screen.debugPrint(30,35, "Return to Homemenu/HBL", white, TOP_SCREEN)
+    Screen.debugPrint(5,5, "encTitleKeysUpdater for freeShop", yellow, TOP_SCREEN)
+    Screen.debugPrint(20,curPos, ">", white, TOP_SCREEN)
+    Screen.debugPrint(30,20, "Download latest .bin", white, TOP_SCREEN)
+    Screen.debugPrint(30,35, "Back to Homemenu", white, TOP_SCREEN)
+    Screen.debugPrint(30,200, "v1.2", white, TOP_SCREEN)
+    Screen.debugPrint(30,215, "by MatMaf", white, TOP_SCREEN)
+    if usersize == size then
+    	Screen.debugPrint(5,20, "encTitleKeys.bin is up to date.", green, BOTTOM_SCREEN)
+    else
+    	Screen.debugPrint(5,20, "encTitleKeys.bin is not up to date.", red, BOTTOM_SCREEN)
+    end
+    Screen.debugPrint(5,5, motd, white, BOTTOM_SCREEN)
     Screen.flip()
 
 	while true do
@@ -90,4 +131,5 @@ function main()
     end
 end
 
+init()
 main()
